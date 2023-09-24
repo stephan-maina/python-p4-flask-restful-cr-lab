b1e1792ead7de95cc645b629123deb9534449174
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-25 01:05:35 +0300

    Completed Lab..

--- server/seed.py
+++ server/seed.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python3
+from app import db
+from models import Newsletter
 
-from app import app
-from models import db, Plant
+def seed_data():
+    with db.app.app_context():
+        db.create_all()
 
-with app.app_context():
+        email1 = Newsletter(email='example1@example.com')
+        email2 = Newsletter(email='example2@example.com')
 
-    Plant.query.delete()
+        db.session.add(email1)
+        db.session.add(email2)
+        db.session.commit()
 
-    aloe = Plant(
-        id=1,
-        name="Aloe",
-        image="./images/aloe.jpg",
-        price=11.50,
-    )
-
-    zz_plant = Plant(
-        id=2,
-        name="ZZ Plant",
-        image="./images/zz-plant.jpg",
-        price=25.98,
-    )
-
-    db.session.add_all([aloe, zz_plant])
-    db.session.commit()
+if __name__ == '__main__':
+    seed_data()
