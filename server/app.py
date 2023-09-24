1d7102cca7d19080fa974e1f8c809182073a97fe
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-25 01:01:41 +0300

    Done..

--- server/app.py
+++ server/app.py
@@ -1,27 +1,35 @@
 #!/usr/bin/env python3
-
-from flask import Flask, jsonify, request, make_response
-from flask_migrate import Migrate
-from flask_restful import Api, Resource
-
-from models import db, Plant
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from models import Newsletter  # Import the Newsletter model
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newsletter.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = True
 
-migrate = Migrate(app, db)
-db.init_app(app)
+db = SQLAlchemy(app)
 
-api = Api(app)
+@app.route('/api/newsletters', methods=['POST'])
+def create_newsletter():
+    data = request.get_json()
+    email = data.get('email')
 
-class Plants(Resource):
-    pass
+    if not email:
+        return jsonify({'message': 'Email is required'}), 400
 
-class PlantByID(Resource):
-    pass
-        
+    # Create a new newsletter entry
+    new_email = Newsletter(email=email)
+    db.session.add(new_email)
+    db.session.commit()
+
+    return jsonify({'message': 'Newsletter subscription created successfully'}), 201
+
+@app.route('/api/newsletters', methods=['GET'])
+def get_newsletters():
+    newsletters = Newsletter.query.all()
+    newsletter_list = [email.to_dict() for email in newsletters]
+
+    return jsonify(newsletter_list)
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(debug=True)
